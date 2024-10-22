a7a886fe4ca3ac61191fa23bfce1c8d77ca3c671
quinver150 <quinver15@gmail.com>
2024-10-22 20:08:17 +0200

    new gitignore

--- Project/obj/Debug/net8.0/Project.AssemblyInfo.cs
+++ Project/obj/Debug/net8.0/Project.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Project")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9cfddd0fa452e528b53914be84644ad300ab690f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6557865d5d3cf6d91d51221dfcc798579f9f718f")]
 [assembly: System.Reflection.AssemblyProductAttribute("Project")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Project")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
