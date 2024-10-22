9cfddd0fa452e528b53914be84644ad300ab690f
quinver150 <quinver15@gmail.com>
2024-10-22 20:01:03 +0200

    init

--- Project/obj/Debug/net8.0/Project.AssemblyInfo.cs
+++ Project/obj/Debug/net8.0/Project.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Project")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+93a26939b3e4a44f2d939fc92698a4b607a61c3f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c8c888bcf575fc4e995f66d1d87ed691efee37c0")]
 [assembly: System.Reflection.AssemblyProductAttribute("Project")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Project")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
