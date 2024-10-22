53039b6f15f4ddb6731764925207b4775212061f
quinver150 <quinver15@gmail.com>
2024-10-22 19:51:31 +0200

    Project folder existst

--- Project/obj/Debug/net8.0/Project.AssemblyInfo.cs
+++ Project/obj/Debug/net8.0/Project.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Project")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c7f130a069a7f42cbe81985852cdd5607a332c25")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+67e334043a7161848e35b6a09db6b6ca7ab21bc9")]
 [assembly: System.Reflection.AssemblyProductAttribute("Project")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Project")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
