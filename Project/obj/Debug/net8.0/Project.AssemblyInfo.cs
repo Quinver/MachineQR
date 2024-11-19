de959386607bcaaad509925c85aeafcedb8a2b65
quinver15 <57844513+Quinver15@users.noreply.github.com>
2024-11-19 13:15:27 +0100

    Update AssemblyInfo and .gitignore; modify Project.AssemblyInfoInputs.cache

--- Project/obj/Debug/net8.0/Project.AssemblyInfo.cs
+++ Project/obj/Debug/net8.0/Project.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Project")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+79e67c3f571283b72415752fa316d3ce72f6320e")]
 [assembly: System.Reflection.AssemblyProductAttribute("Project")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Project")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
