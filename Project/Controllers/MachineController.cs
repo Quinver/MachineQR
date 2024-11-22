5f438a150be7d8179775b5a6512130a6b9ec696b
Finns <Liam.vereycken@tabrasschaat.be>
2024-11-22 15:14:09 +0100

    QR sigma

--- Project/Controllers/MachineController.cs
+++ Project/Controllers/MachineController.cs
@@ -3,6 +3,7 @@ using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Project.Data;
 using Project.Models;
+using QRCoder;
 
 namespace Project.Controllers
 {
@@ -34,5 +35,31 @@ namespace Project.Controllers
             // Pass the machine to the view (Bio.cshtml)
             return View(machine);
         }
+
+        public IActionResult GenerateQRCode(string url)
+        {
+            using (QRCodeGenerator qrGenerator = new QRCodeGenerator())
+            {
+                QRCodeData qrCodeData = qrGenerator.CreateQrCode(url, QRCodeGenerator.ECCLevel.Q);
+                using (QRCode qrCode = new QRCode(qrCodeData))
+                {
+                    using (Bitmap qrCodeImage = qrCode.GetGraphic(20))
+                    {
+                        using (MemoryStream ms = new MemoryStream())
+                        {
+                            qrCodeImage.Save(ms, System.Drawing.Imaging.ImageFormat.Png);
+                            return File(ms.ToArray(), "image/png");
+                        }
+                }
+                
+            }
+        }
+
+        public IActionResult GenerateQr(int machineId)
+        {
+            string url = Url.Action("Bio", "Machine", new { id = machineId }, Request.Scheme);
+            return GenerateQRCode(url);
+        }
+
     }
 }
