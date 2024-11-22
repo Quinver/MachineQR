254e1fc163b24423dbfdabd8a8262595868af6fc
quinver15 <57844513+Quinver@users.noreply.github.com>
2024-11-22 09:50:27 +0100

    Refactor login page by removing unnecessary links and update assembly informational version

--- Project/obj/Debug/net8.0/Project.AssemblyInfo.cs
+++ Project/obj/Debug/net8.0/Project.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Project")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f600702d377b7e34cfcc6fe11d7fc5a9a263ac1e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+de37761204fcbb52d175b7e98ce583cd3327f567")]
 [assembly: System.Reflection.AssemblyProductAttribute("Project")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Project")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
