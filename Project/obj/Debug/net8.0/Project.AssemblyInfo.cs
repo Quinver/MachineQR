257f6b5b30915e6aa5b952996714b2270bb696c7
Finns <Liam.vereycken@tabrasschaat.be>
2024-11-22 15:33:11 +0100

    Qr code

--- Project/obj/Debug/net8.0/Project.AssemblyInfo.cs
+++ Project/obj/Debug/net8.0/Project.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Project")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fbaa790d74a1e2d14391d07823da871e651ef4c0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5f438a150be7d8179775b5a6512130a6b9ec696b")]
 [assembly: System.Reflection.AssemblyProductAttribute("Project")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Project")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
