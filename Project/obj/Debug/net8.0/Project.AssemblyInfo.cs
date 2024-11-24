bbb3f480f32d9dec256dd4f98ebde006d996a517
quinver150 <quinver15@gmail.com>
2024-11-25 00:30:24 +0100

    Bootsrap icons

--- Project/obj/Debug/net8.0/Project.AssemblyInfo.cs
+++ Project/obj/Debug/net8.0/Project.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Project")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+47c1dc2f0ce3f16636a477bbee2e6bc3b2a09531")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c07eb00564f4db0653ac5601263559705f23748c")]
 [assembly: System.Reflection.AssemblyProductAttribute("Project")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Project")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
