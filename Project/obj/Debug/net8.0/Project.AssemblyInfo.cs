ba2e1f8c3b1b5b02cea2a77dad1e041b40f5e273
Hayley <hayley.bachot@tabrasschaat.be>
2024-11-28 13:06:37 +0100

    logo

--- Project/obj/Debug/net8.0/Project.AssemblyInfo.cs
+++ Project/obj/Debug/net8.0/Project.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Project")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e809bb97f64151ef36a8d7317380fc2d3b872189")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+387ffe126f715cf711384b101cddf48b71141668")]
 [assembly: System.Reflection.AssemblyProductAttribute("Project")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Project")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
