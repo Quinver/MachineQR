477c84cc8467da1ce1b3a1ae1bef589c2cb8546d
Hayley <hayley.bachot@tabrasschaat.be>
2024-11-28 13:31:54 +0100

    small adjustments

--- Project/obj/Debug/net8.0/Project.AssemblyInfo.cs
+++ Project/obj/Debug/net8.0/Project.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Project")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+387ffe126f715cf711384b101cddf48b71141668")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c475ab547419efb50dce54750dac064d96a10c66")]
 [assembly: System.Reflection.AssemblyProductAttribute("Project")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Project")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
